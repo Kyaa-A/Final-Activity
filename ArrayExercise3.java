1b63c64bba0f37de0be89db01aaf77824c7c5098
Kyaa-A <asnaripacalna@gmail.com>
2023-03-05 15:56:31 +0800

    init

---
+++ ArrayExercise3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ArrayExercise3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter number of Rows: ");
+        int row = scan.nextInt();
+
+        System.out.print("Enter number of Columns: ");
+        int column = scan.nextInt();
+
+        int table[][] = new int[row+1][column+1];
+
+        for (int i = 1; i < table.length; i++) {
+            for (int j = 1; j < table[i].length; j++) {
+                System.out.printf("%-4d", table[i][j]=(i)*(j));
+            }
+            System.out.println();
+        }
+    }   
+}
