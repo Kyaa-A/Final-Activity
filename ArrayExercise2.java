2fea4dce164eb1161257489ccb2365afafe2498b
Kyaa-A <asnaripacalna@gmail.com>
2023-03-05 16:10:56 +0800

    init

--- ArrayExercise2.java
+++ ArrayExercise2.java
@@ -1,3 +1,16 @@
 public class ArrayExercise2 {
-    
+    public static void main(String[] args) {
+
+        int elements[] = {1,8,10,7,4,112,43,144,18,11};
+
+        for (int i = 0; i < elements.length; i++) {
+            System.out.printf("%4d ",i);
+            
+        }
+        System.out.println("\n" + "-".repeat(50));
+        for (int i = 0; i < elements.length; i++) {
+            System.out.printf("%4d|",elements[i]);           
+        }
+        System.out.println("\n" + "-".repeat(50));
+    }
 }
